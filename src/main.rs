87dce4b9dd2ff49cb91e1a8d1e16a98e7be652f9
Mohammed Saud <md.saud020@gmail.com>
2023-06-07 21:42:27 +0530

    implement basic auto lock event loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,162 @@
-fn main() {
-    println!("Hello, world!");
+use std::{thread, time::Duration};
+
+use wayland_client::{protocol::{wl_registry, wl_compositor, wl_subcompositor, wl_shm}, Connection, Dispatch, QueueHandle};
+use wayland_protocols::ext::session_lock::v1::client::{ext_session_lock_manager_v1, ext_session_lock_v1};
+
+fn main() -> () {
+    let conn = Connection::connect_to_env().unwrap();
+
+    let display = conn.display();
+
+    let mut event_queue = conn.new_event_queue();
+    let qh = event_queue.handle();
+
+    let _registry = display.get_registry(&qh, ());
+
+    let mut app_data = AppData {
+        locked: false,
+        compositor: None,
+        subcompositor: None,
+        shm: None,
+        lock_mgr: None,
+    };
+    event_queue.roundtrip(&mut app_data).unwrap();
+
+    if app_data.lock_mgr.is_none() {
+        panic!("Unable to get lock manager!");
+    }
+
+    let lock = app_data.lock_mgr.as_ref().unwrap().lock(&qh, ());
+    event_queue.roundtrip(&mut app_data).unwrap();
+
+    println!("Sleeping...");
+    thread::sleep(Duration::from_millis(4000));
+    println!("Attempting unlock.");
+    lock.unlock_and_destroy();
+    event_queue.roundtrip(&mut app_data).unwrap();
+    println!("Successful!!!");
+}
+
+struct AppData {
+    locked: bool,
+    compositor: Option<wl_compositor::WlCompositor>,
+    subcompositor: Option<wl_subcompositor::WlSubcompositor>,
+    shm: Option<wl_shm::WlShm>,
+    lock_mgr: Option<ext_session_lock_manager_v1::ExtSessionLockManagerV1>,
+}
+
+impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
+    fn event(
+        state: &mut Self,
+        registry: &wl_registry::WlRegistry,
+        event: wl_registry::Event,
+        _: &(),
+        _: &Connection,
+        qh: &QueueHandle<Self>,
+        ) {
+        if let wl_registry::Event::Global {
+            name,
+            interface,
+            ..
+        } = event
+        {
+            // println!("[{}] {} (v{})", name, interface, version);
+            match &interface[..] {
+                "wl_compositor" => {
+                    let compositor =
+                        registry.bind::<wl_compositor::WlCompositor, _, _>(name, 4, qh, ());
+                    state.compositor = Some(compositor);
+                }
+                "wl_subcompositor" => {
+                    let subcompositor =
+                        registry.bind::<wl_subcompositor::WlSubcompositor, _, _>(name, 1, qh, ());
+                    state.subcompositor = Some(subcompositor);
+                }
+                "wl_shm" => {
+                    let shm = registry.bind::<wl_shm::WlShm, _, _>(name, 1, qh, ());
+                    state.shm = Some(shm);
+                }
+                "ext_session_lock_manager_v1" => {
+                    let lock_mgr = registry.bind::<ext_session_lock_manager_v1::ExtSessionLockManagerV1, _, _>(name, 1, qh, ());
+                    state.lock_mgr = Some(lock_mgr);
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+impl Dispatch<wl_compositor::WlCompositor, ()> for AppData {
+    fn event(
+        _: &mut Self,
+        _: &wl_compositor::WlCompositor,
+        _: wl_compositor::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        ) {
+        // wl_compositor has no event
+    }
+}
+
+impl Dispatch<wl_subcompositor::WlSubcompositor, ()> for AppData {
+    fn event(
+        _: &mut Self,
+        _: &wl_subcompositor::WlSubcompositor,
+        _: wl_subcompositor::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        ) {
+        // wl_compositor has no event
+    }
+}
+
+impl Dispatch<wl_shm::WlShm, ()> for AppData {
+    fn event(
+        _: &mut Self,
+        _: &wl_shm::WlShm,
+        _: wl_shm::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        ) {
+        // we ignore wl_shm events in this example
+    }
+}
+
+impl Dispatch<ext_session_lock_manager_v1::ExtSessionLockManagerV1, ()> for AppData {
+    fn event(
+        _: &mut Self,
+        _: &ext_session_lock_manager_v1::ExtSessionLockManagerV1,
+        _: ext_session_lock_manager_v1::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<AppData>,
+        ) {
+        // todo
+    }
+}
+
+impl Dispatch<ext_session_lock_v1::ExtSessionLockV1, ()> for AppData {
+    fn event(
+        state: &mut Self,
+        _: &ext_session_lock_v1::ExtSessionLockV1,
+        event: ext_session_lock_v1::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<AppData>,
+        ) {
+        match event {
+            ext_session_lock_v1::Event::Finished => {
+                panic!("Unable to lock session!");
+            }
+            ext_session_lock_v1::Event::Locked => {
+                state.locked = true;
+                println!("Session successfully locked!!");
+
+            }
+            _ => {}
+        }
+    }
 }
